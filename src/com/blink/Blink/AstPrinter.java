d1e7c4a617b97b9014ec8e7ab133e72766a1ba8c
Arrush <arrushchaturvedi@gmail.com>
2019-09-15 21:18:32 +0100

    Change to Visitor Interface
    Made default methods so no subclass will have to unnecessarily inherit it.

--- src/com/blink/Blink/AstPrinter.java
+++ src/com/blink/Blink/AstPrinter.java
@@ -4,23 +4,8 @@ class AstPrinter implements Expr.Visitor<String> {
     String print(Expr expr) { return expr.accept(this); }
 
     @Override
-    public String visitAssignExpr(Expr.Assign expr) {
-        return null;
-    }
-
-    @Override
     public String visitBinaryExpr(Expr.Binary expr) {
         return parenthesize(expr.operator.lexeme, expr.left, expr.right);
-    }
-
-    @Override
-    public String visitCallExpr(Expr.Call expr) {
-        return null;
-    }
-
-    @Override
-    public String visitGetExpr(Expr.Get expr) {
-        return null;
     }
 
     @Override
@@ -32,26 +17,6 @@ class AstPrinter implements Expr.Visitor<String> {
     public String visitLiteralExpr(Expr.Literal expr) {
         if (expr.value == null) return "nil";
         return expr.value.toString();
-    }
-
-    @Override
-    public String visitLogicalExpr(Expr.Logical expr) {
-        return null;
-    }
-
-    @Override
-    public String visitSetExpr(Expr.Set expr) {
-        return null;
-    }
-
-    @Override
-    public String visitSuperExpr(Expr.Super expr) {
-        return null;
-    }
-
-    @Override
-    public String visitThisExpr(Expr.This expr) {
-        return null;
     }
 
     @Override
--- src/com/blink/Blink/Expr.java
+++ src/com/blink/Blink/Expr.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 abstract class Expr {
   interface Visitor<R> {
-    R visitAssignExpr(Assign expr);
-    R visitBinaryExpr(Binary expr);
-    R visitCallExpr(Call expr);
-    R visitGetExpr(Get expr);
-    R visitGroupingExpr(Grouping expr);
-    R visitLiteralExpr(Literal expr);
-    R visitLogicalExpr(Logical expr);
-    R visitSetExpr(Set expr);
-    R visitSuperExpr(Super expr);
-    R visitThisExpr(This expr);
-    R visitUnaryExpr(Unary expr);
-    R visitVariableExpr(Variable expr);
+    default R visitAssignExpr(Assign expr) {return null;}
+    default R visitBinaryExpr(Binary expr) {return null;}
+    default R visitCallExpr(Call expr) {return null;}
+    default R visitGetExpr(Get expr) {return null;}
+    default R visitGroupingExpr(Grouping expr) {return null;}
+    default R visitLiteralExpr(Literal expr) {return null;}
+    default R visitLogicalExpr(Logical expr) {return null;}
+    default R visitSetExpr(Set expr) {return null;}
+    default R visitSuperExpr(Super expr) {return null;}
+    default R visitThisExpr(This expr) {return null;}
+    default R visitUnaryExpr(Unary expr) {return null;}
+    default R visitVariableExpr(Variable expr) {return null;}
   }
   static class Assign extends Expr {
     Assign(Token name, Expr value) {
